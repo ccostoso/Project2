3438d58674ac4b05d0b51829528eb422a87d042b
Grace Cho <ekcho3650@gmail.com>
2020-02-23 22:42:36 -0500

    matched id

--- controllers/clause_controller.js
+++ controllers/clause_controller.js
@@ -31,7 +31,7 @@ router.get("/", function (req, res) {
     });
 });
 
-router.get("/:clause_title", function (req, res) {
+router.get("/clause/:clause_title", function (req, res) {
 
   db.Clause.findOne({
     where: {
@@ -91,4 +91,45 @@ router.post("/api/clause", function (req, res) {
 // });
 
 // Export routes for server.js to use.
+
+
+router.get("/book", function (req, res) {
+  res.render("book");//핸들바 불러옴
+});
+
+router.get("/book/:bookTitle", function (req, res) {
+  console.log(req.params.bookTitle)//bookTitle안에는 스페이스가 언더스코어로 바꿔져있는 결과가 출력된다. 
+  db.Book.findOne({
+    where: {
+      book_name: req.params.bookTitle.replace(/_/g, " ")
+      //bookTitle이 url의 param이어서 app.js에서 스페이스를 언더스코어로 바꿔줬지만 
+      //실제 책 이름은 검색할때 다시 언더스코어를 스페이스로 바꿔줘야해서 반대로 리플레이스를 작성
+    }
+  })
+    .then(function (data) {
+      console.log('working2');
+
+      var bookObject = {
+        find_book: data ? (data.dataValues ? data.dataValues : null) : null
+        //data와 data.dataValues가 존재하면 find_book에 data.dataValues를 넣고 아니면 null  
+        //find_book: data.dataValues 
+      };
+
+      res.render("book_result", bookObject);
+
+      console.log(bookObject);
+    });
+});
+
+router.post("/book", function (req, res) {
+  console.log("req.body", req.body);
+
+  db.Book.create({
+    book_name: req.body.book_name,
+    book_requires: req.body.price
+  }).then(function () {
+    res.send(204).end();
+  });
+});
+
 module.exports = router;
